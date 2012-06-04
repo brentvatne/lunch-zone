7ffe533d893977b545ef0aee78b4f0beac48b39a
Brent Vatne <brentvatne@gmail.com>
2012-06-03 19:15:57 -0700

    able to add and get individual restaurants

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,19 +3,32 @@ require_relative '../models/restaurant'
 
 module LunchZone
   class App < Sinatra::Application
+
+    # [ { :name => 'restaurant name', :id => 1232 }, { ... } ]
     get '/api/restaurants' do
       Restaurant.all.map(&:attributes).to_json
-      # [ { :name => 'restaurant name', :id => 1232 }, { ... } ]
     end
 
-    get '/api/restaurants/:date' do
-      # [ { :name => 'restaurant name', :id => 1232,
-      #     :people => [ {:nickname => '..', # :image => 'image_url' }, { ... }, { ... } ],
-      #   { ... } ]
+    # { :name => 'restaurant name', :id => 1232 }
+    get '/api/restaurants/:id' do
+      restaurant = Restaurant.get(params[:id])
+
+      if restaurant
+        restaurant.attributes.to_json
+      else
+        error 404, {:error => 'restaurant not found'}.to_json
+      end
     end
 
+    # { :name => 'some name', :id => 123, :people => [] }
     post '/api/restaurants' do
-      # { :name => 'some name', :id => 123, :people => [] }
+      Restaurant.create(:name => json_params[:name]).attributes.to_json
+    end
+
+    # [ { :name => 'restaurant name', :id => 1232,
+    #     :people => [ {:nickname => '..', # :image => 'image_url' }, { ... }, { ... } ],
+    #   { ... } ]
+    get '/api/restaurants/:date' do
     end
 
     post '/api/restaurants/:id/willing' do
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -12,18 +12,49 @@ module LunchZone
       App
     end
 
+    def parsed_response
+      JSON.parse(last_response.body)
+    end
+
+    before do
+      Restaurant.create(:name => 'QQ Sushi')
+      Restaurant.create(:name => 'Memphis Grill')
+    end
+
     describe 'GET /api/restaurants' do
-      before do
-        Restaurant.create(:name => 'QQ Sushi')
-        Restaurant.create(:name => 'Memphis Grill')
+      it 'returns all restaurants' do
+        get '/api/restaurants'
+
+        last_response.should be_ok
+        first_restaurant = parsed_response.first
+        first_restaurant['name'].should == 'QQ Sushi'
+      end
+    end
+
+    describe 'GET /api/restaurants/:id' do
+      it 'returns info for the restaurant with the given id' do
+        get "/api/restaurants/#{Restaurant.first.id}"
+
+        last_response.should be_ok
+        parsed_response['name'].should == Restaurant.first.name
       end
 
-      it 'gets all restaurants' do
-        get '/api/restaurants'
-        last_response.should be_ok
-        attributes = JSON.parse(last_response.body)
-        first_restaurant = attributes.first
-        first_restaurant['name'].should == 'QQ Sushi'
+      it 'returns 404 with an error message if not found' do
+        get '/api/restaurants/this-is-so-wrong!!!!'
+
+        last_response.status.should == 404
+        parsed_response['error'].should match(/not found/)
+      end
+    end
+
+    describe 'POST /api/restaurants' do
+      it 'creates a new restaurant' do
+        post '/api/restaurants', { :name => 'Some New One' }.to_json
+
+        id = parsed_response['id']
+
+        get "/api/restaurants/#{id}"
+        parsed_response['name'].should == 'Some New One'
       end
     end
   end
